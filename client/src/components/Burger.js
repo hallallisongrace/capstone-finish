fb974493b99d2e855f9b610f125df1d2c5e38bb5
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-06-17 11:13:46 -0400

    update

--- client/src/components/Burger.js
+++ client/src/components/Burger.js
@@ -10,58 +10,58 @@ return (
 <div>
   <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div className="container-fluid bg-light">
-      <Link className="navbar-brand" to="#">Cincy Travels</Link>
+      <a className="navbar-brand" to="#">Cincy Travels</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon" />
       </button>
       <div className="collapse navbar-collapse text-center navbar-nav-scroll" id="navbarSupportedContent">
         <ul className="navbar-nav mx-auto mb-2 mb-lg-0 bg-light">
           <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            <a className="nav-link active" aria-current="page" to="/">Home</a>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
+            <a className="nav-link" to="/about">About</a>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact</Link>
+            <a className="nav-link" to="/contact">Contact</a>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/add">Add Attractions</Link>
+            <a className="nav-link" to="/add">Add Attractions</a>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/attractions">View Attractions</Link>
+            <a className="nav-link" to="/attractions">View Attractions</a>
           </li>
           <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Seasons
-            </Link>
+            </a>
             <ul className="dropdown-menu" aria-labelledby="navbarDropdown1">
-              <li><Link className="dropdown-item" to="https://cincinnatiusa.com/trip-ideas/spring">Spring</Link></li>
-              <li><Link className="dropdown-item" to="https://cincinnatiusa.com/trip-ideas/summer">Summer</Link></li>
-              <li><Link className="dropdown-item" to="https://cincinnatiusa.com/trip-ideas/fall">Autumn</Link></li>
-              <li><Link className="dropdown-item" to="https://cincinnatiusa.com/trip-ideas/winter">Winter</Link></li>
+              <li><a className="dropdown-item" to="https://cincinnatiusa.com/trip-ideas/spring">Spring</a></li>
+              <li><a className="dropdown-item" to="https://cincinnatiusa.com/trip-ideas/summer">Summer</a></li>
+              <li><a className="dropdown-item" to="https://cincinnatiusa.com/trip-ideas/fall">Autumn</a></li>
+              <li><a className="dropdown-item" to="https://cincinnatiusa.com/trip-ideas/winter">Winter</a></li>
             </ul>
             </li>
             <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a className="nav-link dropdown-toggle" to="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Destinations
-            </Link>
+            </a>
             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><Link className="dropdown-item" to="https://wandercincinnati.com/2019/12/01/over-the-rhine-cincinnati/">Over the Rhine</Link></li>
-              <li><Link className="dropdown-item" to="https://www.cincinnatiparks.com/river-parks/smale-riverfront-park/">Smale Riverfront Park</Link></li>
-              <li><Link className="dropdown-item" to="https://www.downtowncincinnati.com/">Downtown</Link></li>
-              <li><Link className="dropdown-item" to="https://www.meetnky.com/">Covington/Northern Kentucky</Link></li>
+              <li><a className="dropdown-item" to="https://wandercincinnati.com/2019/12/01/over-the-rhine-cincinnati/">Over the Rhine</a></li>
+              <li><a className="dropdown-item" to="https://www.cincinnatiparks.com/river-parks/smale-riverfront-park/">Smale Riverfront Park</a></li>
+              <li><a className="dropdown-item" to="https://www.downtowncincinnati.com/">Downtown</a></li>
+              <li><a className="dropdown-item" to="https://www.meetnky.com/">Covington/Northern Kentucky</a></li>
             </ul>
             </li>
             <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown3" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a className="nav-link dropdown-toggle" to="#" id="navbarDropdown3" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Experiences
-            </Link>
+            </a>
             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><Link className="dropdown-item" to="https://www.citybeat.com/arts-culture">Culture</Link></li>
-              <li><Link className="dropdown-item" to="https://www.tripadvisor.com/Attractions-g5503747-Activities-c61-The_Cincinnati_Region.html">Outdoors</Link></li>
-              <li><Link className="dropdown-item" to="https://photos.citybeat.com/25-essential-foods-every-cincinnati-visitor-should-try/?slide=1&admin-ajaxwew">Food</Link></li>
-              <li><Link className="dropdown-item" to="https://anniewearsit.com/cincinnati-official-travel-guide/">Luxury</Link></li>
+              <li><a className="dropdown-item" to="https://www.citybeat.com/arts-culture">Culture</a></li>
+              <li><a className="dropdown-item" to="https://www.tripadvisor.com/Attractions-g5503747-Activities-c61-The_Cincinnati_Region.html">Outdoors</a></li>
+              <li><a className="dropdown-item" to="https://photos.citybeat.com/25-essential-foods-every-cincinnati-visitor-should-try/?slide=1&admin-ajaxwew">Food</a></li>
+              <li><a className="dropdown-item" to="https://anniewearsit.com/cincinnati-official-travel-guide/">Luxury</a></li>
             </ul>
           </li>
         </ul>
--- client/src/footer.js
+++ client/src/footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './footer.css'
 // import github from './assets/assets/githubicon.png';
@@ -33,55 +33,55 @@ class Footer extends React.Component
     <div className="footer-columns">
      <div ClassName="footeroptions" id="footer-left">
      <ul className="nav-item">
-      <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+      <a className="nav-link active" aria-current="page" href="/">Home</a>
       </ul>
         <ul className="nav-item">
-              <Link className="nav-link" href="/about">About</Link>
+              <a className="nav-link" href="/about">About</a>
         </ul>
         <ul className="nav-item">
-              <Link className="nav-link" href="/contact">Contact</Link>
+              <a className="nav-link" href="/contact">Contact</a>
         </ul>
         <ul className="nav-item">
-              <Link className="nav-link" href="/add">Add Attractions</Link>
+              <a className="nav-link" href="/add">Add Attractions</a>
         </ul>
 
       </div>
       <div className="dropdown-container">
  <ul className="nav-item dropdown">
-              <Link className="nav-link dropdown-toggle" href="/#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="/#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Seasons
-              </Link>
+              </a>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown1">
-                <ul><Link className="dropdown-item" href="https://cincinnatiusa.com/trip-ideas/spring">Spring</Link></ul>
-                <ul><Link className="dropdown-item" href="https://cincinnatiusa.com/trip-ideas/summer">Summer</Link></ul>
-                <ul><Link className="dropdown-item" href="https://cincinnatiusa.com/trip-ideas/fall">Autumn</Link></ul>
-                <ul><Link className="dropdown-item" href="https://cincinnatiusa.com/trip-ideas/winter">Winter</Link></ul>
+                <ul><a className="dropdown-item" href="https://cincinnatiusa.com/trip-ideas/spring">Spring</a></ul>
+                <ul><a className="dropdown-item" href="https://cincinnatiusa.com/trip-ideas/summer">Summer</a></ul>
+                <ul><a className="dropdown-item" href="https://cincinnatiusa.com/trip-ideas/fall">Autumn</a></ul>
+                <ul><a className="dropdown-item" href="https://cincinnatiusa.com/trip-ideas/winter">Winter</a></ul>
               </ul>
               </ul>
               <ul className="nav-item dropdown">
-              <Link className="nav-link dropdown-toggle" href="/#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="/#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Destinations
-              </Link>
+              </a>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <ul><Link className="dropdown-item" href="https://wandercincinnati.com/2019/12/01/over-the-rhine-cincinnati/">Over the Rhine</Link></ul>
-                <ul><Link className="dropdown-item" href="https://www.cincinnatiparks.com/river-parks/smale-riverfront-park/">Smale Riverfront Park</Link></ul>
-                <ul><Link className="dropdown-item" href="https://www.downtowncincinnati.com/">Downtown</Link></ul>
-                <ul><Link className="dropdown-item" href="https://www.meetnky.com/">Covington/Northern Kentucky</Link></ul>
+                <ul><a className="dropdown-item" href="https://wandercincinnati.com/2019/12/01/over-the-rhine-cincinnati/">Over the Rhine</a></ul>
+                <ul><a className="dropdown-item" href="https://www.cincinnatiparks.com/river-parks/smale-riverfront-park/">Smale Riverfront Park</a></ul>
+                <ul><a className="dropdown-item" href="https://www.downtowncincinnati.com/">Downtown</a></ul>
+                <ul><a className="dropdown-item" href="https://www.meetnky.com/">Covington/Northern Kentucky</a></ul>
               </ul>
               </ul>
               <ul className="nav-item dropdown">
-              <Link className="nav-link dropdown-toggle" href="/#" id="navbarDropdown3" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="/#" id="navbarDropdown3" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Experiences
-              </Link>
+              </a>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <ul><Link className="dropdown-item" href="https://www.citybeat.com/arts-culture">Culture</Link></ul>
-                <ul><Link className="dropdown-item" href="https://www.tripadvisor.com/Attractions-g5503747-Activities-c61-The_Cincinnati_Region.html">Outdoors</Link></ul>
-                <ul><Link className="dropdown-item" href="https://photos.citybeat.com/25-essential-foods-every-cincinnati-visitor-should-try/?slide=1&admin-ajaxwew">Food</Link></ul>
-                <ul><Link className="dropdown-item" href="https://anniewearsit.com/cincinnati-official-travel-guide/">Luxury</Link></ul>
+                <ul><a className="dropdown-item" href="https://www.citybeat.com/arts-culture">Culture</a></ul>
+                <ul><a className="dropdown-item" href="https://www.tripadvisor.com/Attractions-g5503747-Activities-c61-The_Cincinnati_Region.html">Outdoors</a></ul>
+                <ul><a className="dropdown-item" href="https://photos.citybeat.com/25-essential-foods-every-cincinnati-visitor-should-try/?slide=1&admin-ajaxwew">Food</a></ul>
+                <ul><a className="dropdown-item" href="https://anniewearsit.com/cincinnati-official-travel-guide/">Luxury</a></ul>
               </ul>
             </ul>
             <ul className="nav-item">
-              <Link className="nav-link" href="/attractions">View Attractions</Link>
+              <a className="nav-link" href="/attractions">View Attractions</a>
          </ul>
     
           {/* <h3 id="kable-title"></h3> */}
@@ -99,8 +99,8 @@ class Footer extends React.Component
     <h3 id="dom-title">Domynique Shelby</h3>
     
     <div className="shelby-icons">
-    <Link href="https://github.com/dshelby24" ><FontAwesomeIcon icon={faGithub}/></Link> 
-    <Link href="https://www.linkedin.com/in/domynique-v-shelby/" ><FontAwesomeIcon icon={faLinkedin}/></Link> 
+    <a href="https://github.com/dshelby24" ><FontAwesomeIcon icon={faGithub}/></a> 
+    <a href="https://www.linkedin.com/in/domynique-v-shelby/" ><FontAwesomeIcon icon={faLinkedin}/></a> 
     </div>
     </div>
     
@@ -110,8 +110,8 @@ class Footer extends React.Component
       <h3 id="phil-title">Philicia Watson</h3>
     </div>
     <div className="watson-icons">
-        <Link href="https://github.com/PhiliciaWatson" ><FontAwesomeIcon icon={faGithub}/></Link> 
-        <Link href="https://www.linkedin.com/in/philicia-watson-b26bba1b1/" ><FontAwesomeIcon icon={faLinkedin}/></Link> 
+        <a href="https://github.com/PhiliciaWatson" ><FontAwesomeIcon icon={faGithub}/></a> 
+        <a href="https://www.linkedin.com/in/philicia-watson-b26bba1b1/" ><FontAwesomeIcon icon={faLinkedin}/></a> 
     </div>
     </div>
 
@@ -120,8 +120,8 @@ class Footer extends React.Component
     <h3 id="all-title">Allison Hall</h3>
     </div>
     <div className="hall-icons">
-    <Link href="https://github.com/hallallisongrace" ><FontAwesomeIcon icon={faGithub}/></Link> 
-    <Link href="https://linkedin.com/in/hallallisongrace" ><FontAwesomeIcon icon={faLinkedin}/></Link> 
+    <a href="https://github.com/hallallisongrace" ><FontAwesomeIcon icon={faGithub}/></a> 
+    <a href="https://linkedin.com/in/hallallisongrace" ><FontAwesomeIcon icon={faLinkedin}/></a> 
      </div>
      </div>
     
